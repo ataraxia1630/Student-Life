457f380be909d1cab1907bdd00d436a043d30632
nhittyn <hudinhdong@gmail.com>
2026-03-10 08:09:46 +0700

    feat: implement CardData scriptable object and Enums

---
+++ Assets/_Project/Scripts/Data/CardData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DoiSinhVien.Data
+{
+    [CreateAssetMenu(fileName = "NewCard", menuName = "Student Life/Card Data")]
+    public class CardData : ScriptableObject
+    {
+        [Header("General Information")]
+        public string id; 
+        public string cardName; 
+
+        [TextArea(2, 3)]
+        public string description; 
+
+        [TextArea(2, 3)]
+        public string flavorText; 
+
+        [Header("Stats")]
+        public int manaCost; 
+        public CardType type;
+        public CardRarity rarity;
+
+        [Header("Mechanics")]
+        public bool isExhaust; 
+        public CardData upgradedVersion; 
+
+        [Header("Visuals")]
+        public Sprite artwork; 
+    }
+}
---
+++ Assets/_Project/Scripts/Data/CardEnums.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DoiSinhVien.Data
+{
+    public enum CardType
+    {
+        Attack, 
+        Defend, 
+        Skill,  
+        Power   
+    }
+
+    public enum CardRarity
+    {
+        Common,
+        Uncommon,
+        Rare
+    }
+}
