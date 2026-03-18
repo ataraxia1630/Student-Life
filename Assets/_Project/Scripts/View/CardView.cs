152b494780d921f321747e52b04823df00e87c51
nhittyn <hudinhdong@gmail.com>
2026-03-18 22:02:44 +0700

    feat: implement HandView and CardView logic

---
+++ Assets/_Project/Scripts/View/CardView.cs
@@ -0,0 +1,30 @@
+﻿using DoiSinhVien.Combat;
+using TMPro;
+using UnityEngine;
+
+namespace DoiSinhVien.View
+{
+    public class CardView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text mana;
+        [SerializeField] private TMP_Text title;
+        [SerializeField] private TMP_Text description;
+        [SerializeField] private SpriteRenderer artwork;
+        [SerializeField] private GameObject wrapper;
+
+        public CardInstance LogicCard { get; private set; }
+        public void Setup(CardInstance logicCard)
+        {
+            LogicCard = logicCard;
+            mana.text = logicCard.CurrentCost.ToString();
+            title.text = logicCard.Data.cardName;
+            description.text = logicCard.Data.description; 
+
+            if (logicCard.Data.artwork != null && artwork != null)
+            {
+                artwork.sprite = logicCard.Data.artwork;
+            }
+        }
+
+    }
+}
---
+++ Assets/_Project/Scripts/View/HandView.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace DoiSinhVien.View
+{
+    public class HandView : MonoBehaviour
+    {
+        [SerializeField] private SplineContainer splineContainer;
+        private readonly List<CardView> cards = new();
+
+        public IEnumerator AddCard(CardView cardView)
+        {
+            cards.Add(cardView);
+            yield return UpdateCardPositions(0.15f);
+        }
+
+        public IEnumerator RemoveCard(CardView cardView)
+        {
+            if (cards.Contains(cardView))
+            {
+                cards.Remove(cardView);
+                yield return UpdateCardPositions(0.15f);
+            }
+        }
+
+        private IEnumerator UpdateCardPositions(float duration)
+        {
+            if (cards.Count == 0) yield break;
+            float cardSpacing = 1f / (cards.Count + 1);
+            float firstCardPosition = 0.5f - (cards.Count - 1) * cardSpacing / 2f;
+
+            Spline spline = splineContainer.Spline;
+            for (int i = 0; i < cards.Count; i++)
+            {
+                float position = firstCardPosition + i * cardSpacing;
+                Vector3 splinePos = spline.EvaluatePosition(position);
+                Vector3 forward = spline.EvaluateTangent(position);
+                Vector3 up = spline.EvaluateUpVector(position);
+                Quaternion rotation = Quaternion.LookRotation(-up, Vector3.Cross(-up, forward).normalized);
+
+                cards[i].transform.DOMove(splinePos + transform.position + 0.01f * i * Vector3.back, duration);
+                cards[i].transform.DORotate(rotation.eulerAngles, duration);
+            }
+            yield return new WaitForSeconds(duration);
+        }
+    }
+}
