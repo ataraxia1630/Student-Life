e9a0a4878cedd98c55fe615b98801cfbeb5fba1a
nhittyn <hudinhdong@gmail.com>
2026-03-19 12:20:36 +0700

    feat: implement Enemy Intent and Action

---
+++ Assets/_Project/Scripts/Data/Enemy Action/EnemyActionData.cs
@@ -0,0 +1,14 @@
+﻿using DoiSinhVien.Core;
+using UnityEngine;
+
+namespace DoiSinhVien.Data
+{
+    public abstract class EnemyActionData : ScriptableObject
+    {
+        public string actionName;
+        public IntentType intentType;
+        public int baseValue; 
+
+        public abstract void Execute(ITargetable self, ITargetable target);
+    }
+}
---
+++ Assets/_Project/Scripts/Data/Enemy Action/EnemyDefendAction.cs
@@ -0,0 +1,15 @@
+﻿using DoiSinhVien.Core;
+using UnityEngine;
+
+namespace DoiSinhVien.Data
+{
+    [CreateAssetMenu(fileName = "Enemy_Defend", menuName = "Student Life/Enemy Actions/Defend")]
+    public class EnemyDefendAction : EnemyActionData
+    {
+        public override void Execute(ITargetable self, ITargetable target)
+        {
+            Debug.Log($"[Quái] Tung chiêu {actionName}!");
+            self.GainBlock(baseValue); 
+        }
+    }
+}
