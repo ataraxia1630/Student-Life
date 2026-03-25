478b1e521cc21a43218507334581d0851776b76e
nhittyn <hudinhdong@gmail.com>
2026-03-25 21:22:04 +0700

    feat: implement Battle Manager

---
+++ Assets/_Project/Scripts/Core/BattleManager.cs
@@ -0,0 +1,75 @@
+﻿using DoiSinhVien.Core;
+using DoiSinhVien.Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoiSinhVien.Core
+{
+    public class BattleManager : MonoBehaviour
+    {
+        public static BattleManager Instance { get; private set; }
+
+        [Header("References")]
+        public EncounterGenerator generator; 
+
+        [Header("Battle Setup")]
+        [Tooltip("Các vị trí (Transform) trống trên Scene để đặt quái vật")]
+        public List<Transform> enemySpawnPoints;
+
+        private List<GameObject> activeEnemiesOnScene = new();
+
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else Destroy(gameObject);
+        }
+
+        public void StartBattle(EncounterBlueprint blueprint)
+        {
+            ClearCurrentEnemies();
+
+            List<EnemyData> spawnedEnemyData = generator.GenerateEncounter(blueprint);
+
+            for (int i = 0; i < spawnedEnemyData.Count; i++)
+            {
+                if (i >= enemySpawnPoints.Count)
+                {
+                    Debug.LogWarning("Không đủ vị trí Spawn cho toàn bộ quái!");
+                    break;
+                }
+
+                GameObject newEnemyObj = Instantiate(
+                    spawnedEnemyData[i].enemyPrefab,
+                    enemySpawnPoints[i].position,
+                    Quaternion.identity
+                );
+
+                newEnemyObj.name = spawnedEnemyData[i].enemyName + $"_{i}";
+
+                activeEnemiesOnScene.Add(newEnemyObj);
+
+                EnemyController controller = newEnemyObj.GetComponent<EnemyController>();
+                if (controller != null)
+                {
+                    controller.data = spawnedEnemyData[i];
+                    controller.Initialize();
+                }
+                else
+                {
+                    Debug.LogWarning($"Prefab {newEnemyObj.name} thiếu component EnemyController!");
+                }
+            }
+
+            Debug.Log($"Đã spawn xong {spawnedEnemyData.Count} quái cho Mai nghênh chiến!");
+        }
+
+        private void ClearCurrentEnemies()
+        {
+            foreach (GameObject enemy in activeEnemiesOnScene)
+            {
+                Destroy(enemy);
+            }
+            activeEnemiesOnScene.Clear();
+        }
+    }
+}
