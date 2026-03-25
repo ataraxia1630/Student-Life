c6b87133f043de8f9a053f3e8447c70d1ce0c08b
nhittyn <hudinhdong@gmail.com>
2026-03-25 21:21:43 +0700

    feat: implement Encounter Generator, Blueprint and Room Node

---
+++ Assets/_Project/Scripts/Core/PCG/RoomNode.cs
@@ -0,0 +1,20 @@
+﻿using DoiSinhVien.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DoiSinhVien.Core
+{
+    public class RoomNode : MonoBehaviour
+    {
+        [Header("Setup")]
+        [Tooltip("Kéo Blueprint bạn muốn test vào đây")]
+        public EncounterBlueprint roomBlueprint;
+
+        public void OnRoomSelected()
+        {
+            Debug.Log($"Đã chọn phòng! Chuẩn bị test Blueprint: {roomBlueprint.blueprintName}");
+
+            BattleManager.Instance.StartBattle(roomBlueprint);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Data/EncounterBlueprint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoiSinhVien.Data
+{
+    [System.Serializable]
+    public class EnemySlot
+    {
+        public List<EnemyTag> requiredTags;
+        public List<EnemyTag> forbiddenTags;
+    }
+
+    [CreateAssetMenu(fileName = "NewBlueprint", menuName = "Student Life/Encounter Blueprint")]
+    public class EncounterBlueprint : ScriptableObject
+    {
+        public string blueprintName;
+        public int targetFloor; 
+
+        public List<EnemySlot> slots;
+    }
+}
+
