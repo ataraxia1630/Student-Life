f5531790c49d6d9a827da018f57461c78ef3ceb1
nhittyn <hudinhdong@gmail.com>
2026-04-29 22:57:09 +0700

    feat: implement relic manager, trigger events

---
+++ Assets/_Project/Scripts/Core/GameEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DoiSinhVien.Combat; 
+
+namespace DoiSinhVien.Core
+{
+    public static class GameEvents
+    {
+        public static Action OnCombatStart;
+        public static Action OnTurnStart;
+        public static Action OnTurnEnd;
+        public static Action<CardInstance> OnCardPlayed; 
+        public static Action<int> OnPlayerTookDamage;    
+        public static Action OnCombatWin;
+        public static Action<int> OnCreditsChanged;
+    }
+}
---
+++ Assets/_Project/Scripts/Core/RelicManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using DoiSinhVien.Data;
+using DoiSinhVien.Combat;
+
+namespace DoiSinhVien.Core
+{
+    public class RelicManager : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GameEvents.OnCombatStart += HandleCombatStart;
+            GameEvents.OnCombatWin += HandleCombatWin;
+            GameEvents.OnCardPlayed += HandleCardPlayed;
+        }
+
+        private void OnDisable()
+        {
+            GameEvents.OnCombatStart -= HandleCombatStart;
+            GameEvents.OnCombatWin -= HandleCombatWin;
+            GameEvents.OnCardPlayed -= HandleCardPlayed;
+        }
+
+        private void TriggerRelics(RelicTrigger trigger, object context = null)
+        {
+            if (PlayerInventory.Instance == null || PlayerInventory.Instance.ownedRelics.Count == 0) return;
+
+            PlayerCharacter player = null;
+            if (CombatManager.Instance != null) player = CombatManager.Instance.player;
+
+            foreach (var relic in PlayerInventory.Instance.ownedRelics)
+            {
+                if (relic.passiveEffects == null) continue;
+
+                foreach (var effect in relic.passiveEffects)
+                {
+                    if (effect.triggerMoment == trigger)
+                    {
+                        effect.Execute(player, context);
+                    }
+                }
+            }
+        }
+
+        private void HandleCombatStart() => TriggerRelics(RelicTrigger.CombatStart);
+        private void HandleCombatWin() => TriggerRelics(RelicTrigger.CombatWin);
+        private void HandleCardPlayed(CardInstance card) => TriggerRelics(RelicTrigger.CardPlayed, card);
+    }
+}
