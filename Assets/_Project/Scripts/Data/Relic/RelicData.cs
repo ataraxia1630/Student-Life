f4536481cb35cb22a44809aa43197be8d68e31c2
nhittyn <hudinhdong@gmail.com>
2026-04-29 22:55:50 +0700

    feat: implement relic and effect data

---
+++ Assets/_Project/Scripts/Data/Relic/RelicData.cs
@@ -0,0 +1,29 @@
+﻿using DoiSinhVien.Core;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoiSinhVien.Data
+{
+    public enum RelicRarity { Common, Rare, Legendary }
+
+    [CreateAssetMenu(fileName = "NewRelic", menuName = "Student Life/Relic Data")]
+    public class RelicData : ScriptableObject, IPurchasable
+    {
+        public string id;
+        public string relicName;
+        [TextArea] public string description;
+        public Sprite icon;
+        public int price;
+        public RelicRarity rarity;
+
+        public List<RelicEffectData> passiveEffects;
+
+        public string ItemName => relicName;
+        public int ItemPrice => price;
+
+        public void OnPurchased()
+        {
+            PlayerInventory.Instance.AddRelic(this);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Data/Relic/RelicEffect/RelicEffectData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using DoiSinhVien.Core;
+
+namespace DoiSinhVien.Data
+{
+    public enum RelicTrigger
+    {
+        CombatStart,
+        TurnStart,
+        TurnEnd,
+        CardPlayed,
+        CombatWin
+    }
+
+    public abstract class RelicEffectData : ScriptableObject
+    {
+        public RelicTrigger triggerMoment;
+
+        public abstract void Execute(PlayerCharacter player, object context = null);
+    }
+}
