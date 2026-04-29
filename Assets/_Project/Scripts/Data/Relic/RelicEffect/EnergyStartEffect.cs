b6fb0362fbb7da9358194b56a3c35e31ef26f070
nhittyn <hudinhdong@gmail.com>
2026-04-29 23:01:08 +0700

    feat: implement some relic effects and create relic SOs

---
+++ Assets/_Project/Scripts/Data/Relic/RelicEffect/EnergyStartEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using DoiSinhVien.Core;
+
+namespace DoiSinhVien.Data
+{
+    [CreateAssetMenu(fileName = "EnergyStartRelic", menuName = "Student Life/Relic Effects/Energy Start")]
+    public class EnergyStartEffect : RelicEffectData
+    {
+        public int bonusEnergy = 1;
+
+        public override void Execute(PlayerCharacter player, object context = null)
+        {
+            if (CombatManager.Instance != null)
+            {
+                CombatManager.Instance.currentEnergy += bonusEnergy;
+                Debug.Log($"[Relic] +{bonusEnergy} Energy.");
+            }
+        }
+    }
+}
