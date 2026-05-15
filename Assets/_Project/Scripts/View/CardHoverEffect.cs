4fc1556572a979baf646f856fb6610d719c49459
nhittyn <hudinhdong@gmail.com>
2026-05-15 08:03:06 +0700

    feat: card hover visual effect

---
+++ Assets/_Project/Scripts/View/CardHoverEffect.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering; 
+
+namespace DoiSinhVien.UI
+{
+    public class CardHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [Header("References")]
+        public RectTransform visualTransform;
+
+        [Tooltip("Kéo cục chứa Sorting Group vào đây")]
+        public SortingGroup sortingGroup;
+
+        [Header("Hover Settings")]
+        public float hoverScale = 1.3f;
+        public float hoverPushUp = 2f;
+        public float smoothSpeed = 15f;
+
+        [Header("Sorting Settings")]
+        public int normalSortOrder = 0;
+        public int hoverSortOrder = 100; 
+
+        private Vector3 _originalScale;
+        private Vector2 _originalLocalPos;
+
+        private Vector3 _targetScale;
+        private Vector2 _targetLocalPos;
+
+        private void Awake()
+        {
+            _originalScale = visualTransform.localScale;
+            _originalLocalPos = visualTransform.localPosition;
+
+            _targetScale = _originalScale;
+            _targetLocalPos = _originalLocalPos;
+
+            if (sortingGroup == null) sortingGroup = GetComponentInChildren<SortingGroup>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (sortingGroup != null)
+            {
+                sortingGroup.sortingOrder = hoverSortOrder;
+            }
+
+            _targetScale = _originalScale * hoverScale;
+            _targetLocalPos = _originalLocalPos + new Vector2(0, hoverPushUp);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (sortingGroup != null)
+            {
+                sortingGroup.sortingOrder = normalSortOrder;
+            }
+
+            _targetScale = _originalScale;
+            _targetLocalPos = _originalLocalPos;
+        }
+
+        private void Update()
+        {
+            visualTransform.localScale = Vector3.Lerp(visualTransform.localScale, _targetScale, Time.deltaTime * smoothSpeed);
+            visualTransform.localPosition = Vector2.Lerp(visualTransform.localPosition, _targetLocalPos, Time.deltaTime * smoothSpeed);
+        }
+    }
+}
